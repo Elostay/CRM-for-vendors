8225cea4ca80fccbe45bad270e1034339c6c21d0
unknown <nastia60203@gmail.com>
2024-09-27 16:27:03 +0300

    add: form

---
+++ src/app/components/button.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { FC, ButtonHTMLAttributes } from 'react';
+import clsx from 'clsx';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  disabled?: boolean;
+}
+
+const Button: FC<ButtonProps> = ({ disabled, ...rest }) => {
+  return (
+    <button
+      {...rest}
+      className={clsx(
+        'py-2.5 px-5 bg-gray-900 text-zinc-50 text-base text-center font-medium rounded',
+        !disabled && 'hover:bg-gray-800 active:bg-gray-950',
+        disabled && 'text-zinc-100',
+      )}
+    />
+  );
+};
+
+export default Button;
